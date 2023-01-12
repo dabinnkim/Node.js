1c06b15316a83971439d4119f794c37ef9d4eec5
kdb <dabin6743@gmail.com>
2023-01-12 11:53:52 +0900

    데이터 서버 전송

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //서버 오픈하는 기본 문법
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}));
 
 app.listen(8080, function(){ //서버를 열어줌
     console.log('listening on 8080')
@@ -17,4 +18,11 @@ app.get('/',function(req,res){
 
 app.get('/write',function(req,res){
     res.sendFile(__dirname + '/write.html');
-})
+});
+
+//POST 요청
+app.post('/add',function(req,res){
+    res.send('전송완료');
+    console.log(req.body.title) //body의 name="title"의 정보
+    console.log(req.body.date)
+});
