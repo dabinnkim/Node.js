010a44c6ac800761b2b3ea2f676d431de3f4787c
dabinnkim <dabin6743@gmail.com>
2023-05-09 22:48:17 +0900

    mongoDB 설치

--- server.js
+++ server.js
@@ -3,28 +3,30 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
 
+//어떤 데이터베이스에 데이터 저장할건지 변수 만들어 주기
+
 const MongoClient = require('mongodb').MongoClient;
 
 
 
-
-
 var db;
-MongoClient.connect('mongodb://kdb:6743@ac-9dpe9ub-shard-00-00.bawt6ey.mongodb.net:27017,ac-9dpe9ub-shard-00-01.bawt6ey.mongodb.net:27017,ac-9dpe9ub-shard-00-02.bawt6ey.mongodb.net:27017/?ssl=true&replicaSet=atlas-4bkihp-shard-0&authSource=admin&retryWrites=true&w=majority',function(error,client){
-if(error) return console.log(error)
+MongoClient.connect('mongodb+srv://kdb:6743@cluster0.bawt6ey.mongodb.net/?retryWrites=true&w=majority', function (에러, client) {
 
-db = client.db('todoapp');
+    //연결되면 할일
+    if (에러) return console.log('에러')
+    db = client.db('todoapp'); //todoapp이라는 db로 연결
 
-db.collection('post').insertOne('저장할데이터',function(에러,결과){
-    console.log('저장완료');
-});
 
-app.listen(8080, function(){ //서버를 열어줌
-    console.log('listening on 8080')
-});
+    db.collection('post').insertOne({이름:'kim', 나이:20}, function (에러, 결과) {
+        console.log('저장완료')
+    });//내가 원하는 데이터 
 
+    app.listen(8080, function () { //서버를 열어줌
+        console.log('listening on 8080')
+    });
 
 })
+
 
 
 //GET 요청
